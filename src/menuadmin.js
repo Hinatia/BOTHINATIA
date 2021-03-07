cef76381cc444cd380402041c70a8cb10b0ebfa7
Hinatia <80221187+Hinatia@users.noreply.github.com>
2021-03-07 18:11:56 -0300

    Add files via upload

---
+++ src/menuadmin.js
@@ -0,0 +1,24 @@
+const menuadmin = (prefix, pushname) => {
+ return `   𝐖𝐞𝐡𝐫𝐦𝐚𝐜𝐡𝐭'𝐗𝐒 | ϟϟ 
+ da adm pro bot faz isso
+ 
+ ◪ COMANDO DOS ADMINS
+▻${prefix}bemvindo 1 
+▻${prefix}marcar (marca todos)
+▻${prefix}d (apaga msg do bot)
+▻${prefix}leave (bot mete o pé)
+▻${prefix}leveling (liga o level)
+▻${prefix}add 5528999...
+▻${prefix}promover @
+▻${prefix}fechargrupo
+▻${prefix}abrirgrupo
+▻${prefix}demote @
+▻${prefix}kick @
+▻${prefix}ping
+▻${prefix}level
+ `
+
+
+}
+
+exports.menuadmin = menuadmin
